8c1be4b7ccff8e12f81943fe94ae85558c8834d1
Warinyourself <warinyourself@gmail.com>
2026-06-11 23:03:35 +0300

    Remove dead duplicate *.glsl ambient module declaration

--- env.d.ts
+++ env.d.ts
@@ -7,11 +7,6 @@ declare module '*.svg' {
   export default component
 }
 
-declare module '*.glsl' {
-  const content: string
-  export default content
-}
-
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     AppIcon: typeof import('./src/components/app/AppIcon.vue')['default']
